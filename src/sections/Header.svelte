203d69dacb3de612020e695b90e0ce12861f7585
Alexander Czigler <dev@ilix.se>
2022-07-17 15:56:19 +0200

    feat: add gravatar

--- src/sections/Header.svelte
+++ src/sections/Header.svelte
@@ -1,4 +1,11 @@
+<script>
+  import { url } from 'gravatar';
+  let gravatarUrl = url('dev@ilix.se', { s: 384 });
+</script>
+
 <div id="header" class="flex flex-col justify-center items-center pt-12 font-josefin-sans">
+  <img src={gravatarUrl} alt="Alexander" class="h-40 rounded-full border border-gray-300" />
+
   <h1
     class="color-zinc-100 block text-5xl text-center font-extralight mt-4 pt-0 leading-10 text-text"
   >
